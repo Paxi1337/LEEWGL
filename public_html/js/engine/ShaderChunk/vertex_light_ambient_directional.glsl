a75a9742eede72efb133c527c4d3b0e0ddc0e947
Lukas Oberbichler <l.oberbichler@gmail.com>
2015-04-04 19:33:17 +0200

    MODIFIED: light calculation working;

--- public_html/js/engine/ShaderChunk/vertex_light_ambient_directional.glsl
+++ public_html/js/engine/ShaderChunk/vertex_light_ambient_directional.glsl
@@ -1,3 +1,3 @@
-highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
-highp float directional = max(dot(transformedNormal.xyz, uLightDirection), 0.0);
+vec4 transformedNormal = normalize(uNormalMatrix * vec4(aVertexNormal, 1.0));
+float directional = max(dot(transformedNormal.xyz, uLightDirection), 0.0);
 vLighting = uAmbient + (uLightColor * directional);
