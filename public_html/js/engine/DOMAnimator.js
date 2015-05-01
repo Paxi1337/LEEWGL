1052e74a12260d56f5cf5545197cda72e2ae5f9d
Lukas Oberbichler <l.oberbichler@gmail.com>
2015-05-01 05:23:56 +0200

    ADDED: general animate function added with support for pixel ended styles like width as well as numbers like opacity;

--- public_html/js/engine/DOMAnimator.js
+++ public_html/js/engine/DOMAnimator.js
@@ -1,48 +1,78 @@
 LEEWGL.DOM.Animator = function() {
-    this.fadeIn = function(element, duration, adjacentFunction) {
-        if (typeof adjacentFunction === 'undefined')
-            adjacentFunction = (function() {});
-
-        if (typeof element.animation === 'undefined')
-            element.animation = {};
-
-        element.animation.value = 0;
-        element.setStyle('display', 'block');
-
-        element.animation.fadeIn = window.setInterval(function() {
-            element.animation.running = true;
-            if (element.animation.value < 100) {
-                element.animation.value++;
-                element.setStyle('opacity', element.animation.value / 100);
-            } else {
-                window.clearInterval(element.animation.fadeIn);
-                element.animation.running = false;
-                adjacentFunction();
-            }
-        }, duration);
+    this.fadeIn = function(element, duration, adjacentFunction, steps) {
+        this.animate(element, {
+            'opacity': 1
+        }, duration, adjacentFunction, steps);
     };
 
-    this.fadeOut = function(element, duration, adjacentFunction) {
+    this.fadeOut = function(element, duration, adjacentFunction, steps) {
+        this.animate(element, {
+            'opacity': 0
+        }, duration, adjacentFunction, steps);
+    };
+
+    this.animate = function(element, properties, duration, adjacentFunction, steps) {
         if (typeof adjacentFunction === 'undefined')
             adjacentFunction = (function() {});
 
         if (typeof element.animation === 'undefined')
             element.animation = {};
 
-        element.animation.value = 100;
+        steps = (typeof steps !== 'undefined') ? steps : 10;
 
-        element.animation.fadeOut = window.setInterval(function() {
-            element.animation.running = true;
-            element.animation.value--;
-            if (element.animation.value > 0) {
-                element.setStyle('opacity', element.animation.value / 100);
-            } else {
-                window.clearInterval(element.animation.fadeOut);
-                element.setStyle('display', 'none');
-                element.animation.running = false;
-                adjacentFunction();
+        var invert = false;
+        var pixel = false;
+
+        for (var property in properties) {
+            element.animation[property] = {};
+            element.animation[property].start = (element.get(property) !== null) ? parseFloat(element.get(property)) : 0;
+            element.animation[property].end = parseFloat(properties[property]);
+
+            if (typeof properties[property] === 'string') {
+                if (properties[property].indexOf('px') !== -1)
+                    pixel = true;
             }
-        }, duration);
+
+            if (element.animation[property].start > element.animation[property].end) {
+                invert = true;
+                element.animation[property].increment = (element.animation[property].start - element.animation[property].end) / steps;
+            } else {
+                element.animation[property].increment = (element.animation[property].end - element.animation[property].start) / steps;
+            }
+
+            element.animation[property].function = window.setInterval(function() {
+                element.animation.running = true;
+                if (invert === true) {
+                    element.animation[property].start -= element.animation[property].increment;
+                    element.animation[property].start = Math.round(element.animation[property].start * 100) / 100;
+                    if (element.animation[property].start >= element.animation[property].end) {
+                        if (pixel === true)
+                            element.setStyle(property, element.animation[property].start + 'px');
+                        else
+                            element.setStyle(property, element.animation[property].start);
+                    } else {
+                        window.clearInterval(element.animation[property].function);
+                        element.animation.running = false;
+                        adjacentFunction();
+                    }
+                } else {
+                    element.animation[property].start += element.animation[property].increment;
+                    element.animation[property].start = Math.round(element.animation[property].start * 100) / 100;
+
+                    if (element.animation[property].start <= element.animation[property].end) {
+                        if (pixel === true)
+                            element.setStyle(property, element.animation[property].start + 'px');
+                        else
+                            element.setStyle(property, element.animation[property].start);
+                    } else {
+                        window.clearInterval(element.animation[property].function);
+                        element.animation.running = false;
+                        adjacentFunction();
+                    }
+                }
+
+            }, (duration * 1000) / steps);
+        }
     };
 
     this.isRunning = function(element) {
